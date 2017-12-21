7d2503e767138dd76b256aba5d3d3ce4aed24def
wencheng <wencheng@wenchengdeiMac.local>
2017-12-21 16:33:34 +0800

    111

---
+++ YiJIaStoreApp/YiJIaStoreApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiJIaStoreApp
+//
+//  Created by wencheng on 2017/12/21.
+//  Copyright © 2017年 com.kaichuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YiJIaStoreApp/YiJIaStoreApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YiJIaStoreApp
+//
+//  Created by wencheng on 2017/12/21.
+//  Copyright © 2017年 com.kaichuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
